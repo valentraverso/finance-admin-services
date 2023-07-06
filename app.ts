6ca4edf49ae858a4936b9cba7b1a8c72ee57c2ed
Valentino Traverso <vt20032003@live.com.ar>
2023-07-06 11:57:00 +0200

    feat: routes

---
+++ app.ts
@@ -0,0 +1,3 @@
+import startServer from "./shared/server";
+
+startServer();
--- shared/server.ts
+++ shared/server.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import { PORT } from "../config/config";
+import ROUTER from "./routes/router";
 
 const app = express();
 
-app.listen()
+app.use('/api/v1', ROUTER);
+
+export default function startServer() {
+    app.listen(PORT, () => {
+        console.log(`SERVER STARTED IN PORT: ${PORT}`)
+    })
+}
